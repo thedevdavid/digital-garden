92f20b49c55efa42e819ad78b4595d102386017c
David Levai <david@davidlevai.com>
2023-07-02 00:41:29 +0200

    feat: using copy button

--- app/(social)/social/page.tsx
+++ app/(social)/social/page.tsx
@@ -2,11 +2,12 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { Github, Instagram, Linkedin, Twitter, Youtube } from "lucide-react";
+import { Github, Instagram, Linkedin, Mail, Twitter, Youtube } from "lucide-react";
 
 import { defaultAuthor } from "@/lib/metadata";
 import { projects } from "@/lib/projects-data";
 import { Button } from "@/components/ui/button";
+import { CopyButton } from "@/components/copy-button";
 import CTA from "@/components/cta";
 import { Signature } from "@/components/signature";
 import { SpotlightCard } from "@/components/spotlight-card";
@@ -14,11 +15,11 @@ import { SpotlightCard } from "@/components/spotlight-card";
 export default async function SocialPage() {
   return (
     <>
-      <div className="container py-10">
+      <div className="container pt-10">
         <div className="flex flex-col items-center justify-center">
           <div className="flex flex-col items-center justify-center md:flex-row">
             <Image src="/avatar-home.png" alt={defaultAuthor.name} width={200} height={200} />
-            <div>
+            <div className="mt-4 text-center md:ml-4 md:text-left">
               <h1 className="font-heading text-4xl font-bold">{defaultAuthor.name}</h1>
               <p>{defaultAuthor.handle}</p>
             </div>
@@ -30,35 +31,38 @@ export default async function SocialPage() {
             ))}
           </div>
           <h2 className="mb-2 mt-6 font-heading text-2xl font-bold">My socials</h2>
-          <div className="flex w-full flex-col space-y-2 text-sm text-muted-foreground">
+          <div className="mb-4 flex w-full max-w-2xl flex-col space-y-4">
             <Button variant="default" asChild>
               <Link href={defaultAuthor.social.twitter} target="_blank">
-                <Twitter className="h-5 w-5 hover:text-foreground" /> My Twitter profile
+                <Twitter /> My Twitter profile
               </Link>
             </Button>
             <Button variant="default" asChild>
               <Link href={defaultAuthor.social.youtube} target="_blank">
-                <Youtube className="h-5 w-5 hover:text-foreground" /> My Youtube channel
+                <Youtube /> My Youtube channel
               </Link>
             </Button>
             <Button variant="default" asChild>
               <Link href={defaultAuthor.social.github} target="_blank">
-                <Github className="h-5 w-5 hover:text-foreground" /> My Github profile
+                <Github /> My Github profile
               </Link>
             </Button>
             <Button variant="default" asChild>
               <Link href={defaultAuthor.social.instagram} target="_blank">
-                <Instagram className="h-5 w-5 hover:text-foreground" /> My Instagram profile
+                <Instagram /> My Instagram profile
               </Link>
             </Button>
             <Button variant="default" asChild>
               <Link href={defaultAuthor.social.linkedin} target="_blank">
-                <Linkedin className="h-5 w-5 hover:text-foreground" /> My LinkedIn profile
+                <Linkedin /> My LinkedIn profile
               </Link>
             </Button>
+            <CopyButton variant="default" copyText={defaultAuthor.email}>
+              <Mail /> Email address
+            </CopyButton>
           </div>
+          <Signature />
         </div>
-        <Signature />
       </div>
       <CTA
         title="I also write deep dives in email"
--- components/footer.tsx
+++ components/footer.tsx
@@ -5,9 +5,10 @@ import Link from "next/link";
 import { Github, Instagram, Linkedin, Mail, Twitter, Youtube } from "lucide-react";
 
 import siteMetadata, { defaultAuthor } from "@/lib/metadata";
-import { Button } from "@/components/ui/button";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { CopyButton } from "@/components/copy-button";
 import { Signature } from "@/components/signature";
+
+import { Button } from "./ui/button";
 
 const Footer = () => {
   return (
@@ -16,39 +17,40 @@ const Footer = () => {
       <div className="container flex flex-col items-center justify-between space-y-5 text-center lg:flex-row lg:space-y-0 lg:text-left">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0 lg:order-2">
           <div className="flex flex-row space-x-2 text-sm text-muted-foreground">
-            <Link href={defaultAuthor.social.twitter} target="_blank">
-              <Twitter className="h-5 w-5 hover:text-foreground" />
-              <span className="sr-only">Go to my Twitter profile</span>
-            </Link>
-            <Link href={defaultAuthor.social.youtube} target="_blank">
-              <Youtube className="h-5 w-5 hover:text-foreground" />
-              <span className="sr-only">Go to my Youtube channel</span>
-            </Link>
-            <Link href={defaultAuthor.social.github} target="_blank">
-              <Github className="h-5 w-5 hover:text-foreground" />
-              <span className="sr-only">Go to my Github profile</span>
-            </Link>
-            <Link href={defaultAuthor.social.instagram} target="_blank">
-              <Instagram className="h-5 w-5 hover:text-foreground" />
-              <span className="sr-only">Go to my Instagram profile</span>
-            </Link>
-            <Link href={defaultAuthor.social.linkedin} target="_blank">
-              <Linkedin className="h-5 w-5 hover:text-foreground" />
-              <span className="sr-only">Go to my LinkedIn profile</span>
-            </Link>
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  variant="ghost"
-                  className="m-0 h-5 w-5 items-start p-0"
-                  onClick={() => navigator.clipboard.writeText(defaultAuthor.email)}
-                >
-                  <Mail className="hover:text-foreground" />
-                  <span className="sr-only">Copy email address</span>
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent>Email copied!</PopoverContent>
-            </Popover>
+            <Button asChild variant="ghost" size="icon" className="hover:text-foreground">
+              <Link href={defaultAuthor.social.twitter} target="_blank">
+                <Twitter />
+                <span className="sr-only">Go to my Twitter profile</span>
+              </Link>
+            </Button>
+            <Button asChild variant="ghost" size="icon" className="hover:text-foreground">
+              <Link href={defaultAuthor.social.youtube} target="_blank">
+                <Youtube />
+                <span className="sr-only">Go to my Youtube channel</span>
+              </Link>
+            </Button>
+            <Button asChild variant="ghost" size="icon" className="hover:text-foreground">
+              <Link href={defaultAuthor.social.github} target="_blank">
+                <Github />
+                <span className="sr-only">Go to my Github profile</span>
+              </Link>
+            </Button>
+            <Button asChild variant="ghost" size="icon" className="hover:text-foreground">
+              <Link href={defaultAuthor.social.instagram} target="_blank">
+                <Instagram />
+                <span className="sr-only">Go to my Instagram profile</span>
+              </Link>
+            </Button>
+            <Button asChild variant="ghost" size="icon" className="hover:text-foreground">
+              <Link href={defaultAuthor.social.linkedin} target="_blank">
+                <Linkedin />
+                <span className="sr-only">Go to my LinkedIn profile</span>
+              </Link>
+            </Button>
+            <CopyButton size="icon" variant="ghost" className="hover:text-foreground" copyText={defaultAuthor.email}>
+              <Mail />
+              <span className="sr-only">Email address</span>
+            </CopyButton>
           </div>
         </div>
         <div className="flex flex-col gap-4 px-8 md:gap-2 md:px-0 lg:order-1">
