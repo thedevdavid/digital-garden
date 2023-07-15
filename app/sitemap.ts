fe3a0e0ed0cc115c72c0fa4a28748578bda2738a
David Levai <david@davidlevai.com>
2023-07-15 19:15:23 +0200

    fix: removed extra about from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -27,10 +27,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: now,
     },
     {
-      url: `${BASE_URL}/about`,
-      lastModified: now,
-    },
-    {
       url: `${BASE_URL}/projects`,
       lastModified: now,
     },
