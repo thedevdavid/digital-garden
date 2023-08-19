9e757841da00f2a6f58c5cfe1f8bd5aaa45d3d5b
David Levai <david@davidlevai.com>
2023-08-19 18:27:42 +0200

    fix: base url

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -3,7 +3,9 @@ import { AuthorType, SiteMetaData } from "@/types";
 import { socialProfiles } from "./social-data";
 
 export const BASE_URL =
-  process.env.VERCEL_URL || process.env.NEXT_PUBLIC_BASE_URL || `http://localhost:${process.env.PORT || 3000}`;
+  `https://${process.env.VERCEL_URL}` ||
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  `http://localhost:${process.env.PORT || 3000}`;
 
 export const defaultAuthor: AuthorType = {
   name: "Amy Shields",
